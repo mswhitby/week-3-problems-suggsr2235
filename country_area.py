4f8c92fac08df76625bcffe7c604faa7edf59994
suggsr2235 <112404820+suggsr2235@users.noreply.github.com>
2022-08-30 09:40:35 -0500

    Update country_area.py
    Dario and Azael helped me work on it

--- country_area.py
+++ country_area.py
@@ -1,1 +1,9 @@
-
+#defines c and n as variables
+def aoc(c,n):
+#the calculations to get the countries % landmass of the earth
+  a = (n/148940000)*100
+  a = round(a,2)
+  #prints the required message back
+  print (c+" is " + str(a) +"% of the world's landmass.")
+    
+aoc('USA',9372610)
