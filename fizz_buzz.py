03f24588547a9a7ffc8d50a16246d9a2bc5b4cd2
suggsr2235 <112404820+suggsr2235@users.noreply.github.com>
2022-08-30 09:12:17 -0500

    Update fizz_buzz.py
    Dario and Azael worked with me

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,1 +1,11 @@
-
+#defines n as the function for fb
+def fb(n):
+  #uses remainders to say that if a number is divisible by 3 then put buzz, if 5 then put Fizz, if both, put FizzBuzz
+  if n%5 == 0 and n%3 == 0:
+   print("FizzBuzz")
+  elif n%3 == 0:
+   print("Buzz")
+  elif n%5 == 0:
+    print ("Fizz")
+   #defines what n is equal to
+fb(15)
