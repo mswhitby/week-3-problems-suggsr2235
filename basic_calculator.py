dbc861209c4c036365da7e4f870e81eeb1537a72
suggsr2235 <112404820+suggsr2235@users.noreply.github.com>
2022-08-30 09:49:54 -0500

    Update basic_calculator.py
    Dario and Azael worked in a group with me to do this

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,1 +1,20 @@
+def calc(n1,o,n2):
+  if o == "+":
+    return(n1+n2)
+    
+  if o == "-":
+    return(n1-n2)
+    
+  if o == "*":
+    return(n1*n2)
+    
+  if o == "/":
+    if n2==0:
+      return("Cant divide by 0!")
+    else:
+      return(n1/n2)
+      
 
+printCalc=calc(50,"/",10)
+print(printCalc)
+#general calculator that can add, subtract, multiply, and divide using function inputs.
