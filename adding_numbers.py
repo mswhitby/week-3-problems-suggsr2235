edb8b74cd2958f13e219c339eb92a356bd745131
suggsr2235 <112404820+suggsr2235@users.noreply.github.com>
2022-08-31 09:47:04 -0500

    Update adding_numbers.py

--- adding_numbers.py
+++ adding_numbers.py
@@ -1,5 +1,14 @@
 def add(n1,n2):
   return n1+n2
-n3=add(1,6)
+n3=add(17,67)
 print(n3)
 #basic code that adds two numbers together
+
+#one done in class
+def add(n1,n2):
+  if (n1 != "" or None) and (n2 != "" or None):
+    answer=(int(n1)+int(n2))
+    print(str(answer))
+  else:
+    print("invalid operation")
+add("15","67")
