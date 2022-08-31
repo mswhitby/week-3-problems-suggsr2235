4888eded6efad519b7a6c52ea27a45ace4a2c158
suggsr2235 <112404820+suggsr2235@users.noreply.github.com>
2022-08-31 09:06:05 -0500

    Update calc_the_profit.py

--- calc_the_profit.py
+++ calc_the_profit.py
@@ -1,5 +1,9 @@
+#lists the functions for cost, sell price, and inventory
 def pro(c,s,i):
+  #equation to calculate profit
   a=(s-c)*i
+  #rounds equation to nearest Hundreth
   a=round(a,2)
+  #prints the profit
   print (str(a) + " is your profit")
 pro(700.553,1000.7322,455)
