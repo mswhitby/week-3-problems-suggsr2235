71d1e06fb7359f6e5370e651147773fa9b78ddaf
suggsr2235 <112404820+suggsr2235@users.noreply.github.com>
2022-08-31 09:53:31 -0500

    Update calc_damage.py
    Dario helped me with the time

--- calc_damage.py
+++ calc_damage.py
@@ -1,1 +1,13 @@
-
+def dam(d,s,t):
+  if t == "second" and d>0 and s>0:
+    print(d*s*1)
+  elif t == "minute" and d>0 and s>0:
+    print(d*s*60)
+  elif t == "hour" and d>0 and s>0:
+    print(d*s*3600)
+  elif t == "day" and d>0 and s>0:
+    print(d*s*86400)
+  else:
+    print("Invalid")
+dam(15,65,"minute")
+  
