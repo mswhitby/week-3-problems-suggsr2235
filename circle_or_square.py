dc15e93619f5727db738581c13d39ed4db10c273
suggsr2235 <112404820+suggsr2235@users.noreply.github.com>
2022-08-31 09:34:32 -0500

    Update circle_or_square.py
    Dario and Azael helped me with parts of it

--- circle_or_square.py
+++ circle_or_square.py
@@ -1,1 +1,12 @@
-
+import math
+#defines the circle and square function
+def cos(c,s):
+  #calculations to get the perimeter of a square and circle (represented by sp and cp repsectively) 
+ sp=s/math.sqrt(s)*4
+ cp= c*2*3.14
+  #states that if the perimeter of the circle is bigger than the square's, print true, if the opposite print false
+ if cp > sp:
+   print ('true')
+ if sp > cp:
+   print ("False")
+cos(1500,1200)
